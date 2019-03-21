0baacb829f0cc3ba5445e5ca943cc5926d2d5fba
futeng2 <futeng2@163.com>
2019-03-21 22:12:03 +0800

    first commit

---
+++ pages/jingdian/jingdian.js
@@ -0,0 +1,91 @@
+// pages/jingdian/jingdian.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    array:[
+      {
+        "img":'../image/list/mao-s.png',
+        "title": '橘子洲',
+        "type": '景点',
+        "liulan": '1232浏览量',
+        "pinglun": '22评论数'
+      },
+      {
+        "img": '../image/list/aiwanting-s.png',
+        "title": '爱晚亭',
+        "type": '景点',
+        "liulan": '232浏览量',
+        "pinglun": '12评论数'
+      }
+    ],
+    indicatorDots:true,
+    autoplay:true,
+    interval:5000,
+    duration:1000,
+    imgUrls:[
+      "../image/poster/mao.png",
+      "../image/poster/taipingjie.png",
+      "../image/poster/aiwanting.png",
+      "../image/poster/mawangdui.png"
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
